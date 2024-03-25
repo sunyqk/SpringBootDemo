049b4154ed25da6b65be8cec9fc32c5d8b17e271
sunyqk <1807550687@qq.com>
2024-03-26 00:33:44 +0800

    提交SpringBootDemo模块中Student类

---
+++ SpringBootTest01/src/main/java/sun/com/springbootapp/pojo/Student.java
@@ -0,0 +1,11 @@
+package sun.com.springbootapp.pojo;/*
+@author sun
+@date 24/03/26
+@project IntelliJ IDEA
+*/
+
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("student");
+    }
+}
